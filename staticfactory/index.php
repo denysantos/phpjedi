e0a4b50e2f30d2dad2e3927d8a03f93bdd08a8e2
denysantos <39014326+denysantos@users.noreply.github.com>
2021-02-18 06:02:40 -0300

    Add files via upload

---
+++ staticfactory/index.php
@@ -0,0 +1,30 @@
+<?php
+final class StaticFactory {
+    public static function make($type) {
+        if($type == 'number') {
+            return new FormatNumber();
+        }
+        if($type == 'string') {
+            return new FormatString();
+        }
+    }
+}
+
+interface FormatterInterface {
+    public function format($n);
+}
+
+class FormatNumber implements FormatterInterface {
+    public function format($n) {
+        echo 'Formatando número '.$n;
+    }
+}
+
+class FormatString implements FormatterInterface {
+    public function format($n) {
+        echo 'Formatando string '.$n;
+    }
+}
+
+$formatter = StaticFactory::make("string");
+$formatter->format("testando 1, 2, 3, ...");
